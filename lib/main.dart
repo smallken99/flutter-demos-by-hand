ed9ed632cc8a9a10dab25840431701a1b63b3569
smallken99 <smallken@gmail.com>
2020-05-05 20:30:03 +0800

    2.2

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,11 @@
 // 在 first_screen.dart 寫一點邏輯,示範
 
 import "package:flutter/material.dart";
-import './app_screens/first_screen.dart';
+import "./app_screens/home.dart";
 
-void main() => runApp(new MyFlutterApp());
-
-class MyFlutterApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: "My Flutter App",
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text("My First App Screen"),
-            ),
-            body: FirstScreen()));
-  }
+void main() {
+  runApp(MaterialApp(
+    title: "Exploring UI widgets",
+    home: Home(),
+  ));
 }
