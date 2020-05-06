a42d41281933bef5bb1c9f90e54013c8a1e8ed7f
smallken99 <smallken@gmail.com>
2020-05-06 20:18:09 +0800

    #2.8 Flutter LONG LIST. Memory Efficient dynamic ListView tutorial: Flutter Dart Tutorial

--- lib/main.dart
+++ lib/main.dart
@@ -9,39 +9,29 @@ void main() {
     title: "Exploring UI widgets",
     home: Scaffold(
       appBar: AppBar(
-        title: Text('Basic List View'),
+        title: Text('Long List'),
       ),
       body: getListView(),
     ),
   ));
 }
 
+List<String> getListElements() {
+  var items = List<String>.generate(1000, (counter) => "Item $counter");
+  return items;
+}
+
 Widget getListView() {
-  var listView = ListView(
-    children: <Widget>[
-      ListTile(
-        leading: Icon(Icons.landscape),
-        title: Text('Landscape'),
-        subtitle: Text('Beautiful View'),
-        trailing: Icon(Icons.wb_sunny),
-        onTap: () {
-          debugPrint("Landscape tapped");
-        },
-      ),
-      ListTile(
-        leading: Icon(Icons.laptop_chromebook),
-        title: Text('Windows'),
-      ),
-      ListTile(
-        leading: Icon(Icons.phone),
-        title: Text('Phone'),
-      ),
-      Text('Yet another element in List'),
-      Container(
-        color: Colors.red,
-        height: 50.0,
-      )
-    ],
-  );
+  var listItems = getListElements();
+
+  var listView = ListView.builder(itemBuilder: (context, index) {
+    return ListTile(
+      leading: Icon(Icons.arrow_right),
+      title: Text(listItems[index]),
+      onTap: () {
+        debugPrint("${listItems[index]} was tapped.");
+      },
+    );
+  });
   return listView;
 }
