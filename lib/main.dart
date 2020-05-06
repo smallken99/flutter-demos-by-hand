901e22791b8fd935681c35e5fbbb76d343611be4
smallken99 <smallken@gmail.com>
2020-05-06 19:58:42 +0800

    #2.7 Flutter: How to create List in Flutter? Flutter ListView Tutorial in Dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,43 @@ import "./app_screens/home.dart";
 
 void main() {
   runApp(MaterialApp(
+    // debugShowCheckedModeBanner: false,
     title: "Exploring UI widgets",
-    home: Home(),
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('Basic List View'),
+      ),
+      body: getListView(),
+    ),
   ));
+}
+
+Widget getListView() {
+  var listView = ListView(
+    children: <Widget>[
+      ListTile(
+        leading: Icon(Icons.landscape),
+        title: Text('Landscape'),
+        subtitle: Text('Beautiful View'),
+        trailing: Icon(Icons.wb_sunny),
+        onTap: () {
+          debugPrint("Landscape tapped");
+        },
+      ),
+      ListTile(
+        leading: Icon(Icons.laptop_chromebook),
+        title: Text('Windows'),
+      ),
+      ListTile(
+        leading: Icon(Icons.phone),
+        title: Text('Phone'),
+      ),
+      Text('Yet another element in List'),
+      Container(
+        color: Colors.red,
+        height: 50.0,
+      )
+    ],
+  );
+  return listView;
 }
